47a438d76bf4e54d91fd388a34e446cef3979ef6
vitttoria <115340422+vitttoria@users.noreply.github.com>
2023-02-14 20:10:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,11 +29,12 @@ class Password:
         password = hashlib.sha256(password.encode()).hexdigest()
         return password
 
-    def check_password(self, password):
-        if password == hashlib.sha256(password.encode()).hexdigest():
-            print("Авторизация прошла успешно")
-        else:
-            print("Пароль неверный")
+    def check_password(self, old_hash, password):
+        if old_hash == hashlib.sha256(password.encode()).hexdigest():
+            if password == hashlib.sha256(password.encode()).hexdigest():
+                print("Авторизация прошла успешно")
+            else:
+                print("Пароль неверный")
 
 
 class Product:
@@ -87,25 +88,28 @@ class Cart:
     def __init__(self):
         self.cart = []
 
-    def add_to_cart(self, products: Product):
-        self.cart.append(products)
+    def add_to_cart(self, product):
+        self.cart.append(product)
 
-    def del_from_cart(self, products: Product):
-        self.cart.remove(products)
+    def del_from_cart(self, product):
+        self.cart.remove(product)
 
     def __str__(self):
+        return f'{self.cart}'
+
+    def __repr__(self):
         return f'{self.cart}'
 
 
 class User:
     id_counter = IdCounter()
-    user_cart = Cart()
     user_password = Password()
 
-    def __init__(self, username):
+    def __init__(self, username, password):
         self._id = self.id_counter.get_id_()
+        self.user_cart = Cart()
         self.cart_ = self.user_cart.cart
-        self.__password = self.user_password.get_hash(input())
+        self.__password = self.user_password.get_hash(password)
         if len(username) > 3:
             if isinstance(username, str):
                 self._username = username
@@ -122,20 +126,26 @@ class User:
 
 
 class Store:
-    user_password = Password()
-    user_cart = Cart()
+    # user_password = Password()
+    # user_cart = Cart()
 
     def __init__(self):
+        self.user = None
+        self.auth()
+        # self.user_cart = Cart()
+        # self.__password = self.user_password.get_hash(input())
+
+    def auth(self):
         self.login = input("Введите логин\n")
-        self.__password = self.user_password.get_hash(input())
+        self.user = User(self.login, password=input("Введите пароль"))
 
     def add_random_product(self):
         random_product = random.choice(open('products.txt', encoding="utf-8").readlines())
-        self.user_cart.add_to_cart(random_product)
-        return f'{self.user_cart}'
+        self.user.user_cart.add_to_cart(random_product)
+        return f'{self.user.user_cart}'
 
     def get_user_cart(self):
-        print(self.user_cart)
+        print(self.user.user_cart)
 
 
 if __name__ == '__main__':
@@ -149,14 +159,13 @@ if __name__ == '__main__':
     # product2.__str__()
 
     """Тест добавления продуктов в корзину и отображения корзины """
-    store = Store()
-    store.add_random_product()
-    print(store.user_cart)
-    store.add_random_product()
-    print(store.user_cart)
+    # store = Store()
+    # store.add_random_product()
+    # print(store.user_cart)
+    # store.add_random_product()
+    # print(store.user_cart)
 
     """Тест класса "Корзина" """
-    # cart = Cart()
-    # cart.add_to_cart(product1)
-    # print(cart)
-
+    cart = Cart()
+    cart.add_to_cart(product1)
+    cart.__str__()
